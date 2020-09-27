31773446c86bd25dbe1116b489cce99bb41fef33
Haosam <36357075+Haosam@users.noreply.github.com>
2020-09-27 15:49:44 +0800

    Update (Regex Substutiton).py

--- python/Regex and Parsing/(Regex Substutiton).py
+++ python/Regex and Parsing/(Regex Substutiton).py
@@ -2,3 +2,43 @@ from sys import stdin
 import re
 n = input()
 print(re.sub( r"(?<= )(&&|\|\|)(?= )", lambda x: "and" if x.group()=="&&" else "or", stdin.read()))
+
+
+
+
+
+
+
+
+
+
+
+/**
+Sample Input
+
+11
+a = 1;
+b = input();
+
+if a + b > 0 && a - b < 0:
+    start()
+elif a*b > 10 || a/b < 1:
+    stop()
+print set(list(a)) | set(list(b)) 
+#Note do not change &&& or ||| or & or |
+#Only change those '&&' which have space on both sides.
+#Only change those '|| which have space on both sides.
+Sample Output
+
+a = 1;
+b = input();
+
+if a + b > 0 and a - b < 0:
+    start()
+elif a*b > 10 or a/b < 1:
+    stop()
+print set(list(a)) | set(list(b)) 
+#Note do not change &&& or ||| or & or |
+#Only change those '&&' which have space on both sides.
+#Only change those '|| which have space on both sides.    
+**/
